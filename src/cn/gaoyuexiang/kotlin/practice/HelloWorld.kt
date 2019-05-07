b8699de9387be4232f7ca68600692fc831231603
kbyyd24 <melo@gaoyuexiang.cn>
2019-05-07 23:47:04 +0800

    add class Auther

--- src/cn/gaoyuexiang/kotlin/practice/HelloWorld.kt
+++ src/cn/gaoyuexiang/kotlin/practice/HelloWorld.kt
@@ -1,6 +1,7 @@
 package cn.gaoyuexiang.kotlin.practice
 
 import cn.gaoyuexiang.kotlin.practice.Color.GREEN
+import cn.gaoyuexiang.kotlin.practice.about.Auther
 import cn.gaoyuexiang.kotlin.practice.extensions.lastChar
 
 fun main() {
@@ -9,4 +10,10 @@ fun main() {
     println(max(a))
     println(level(GREEN))
     println("Kotlin".lastChar())
+    objectOrientation()
+}
+
+fun objectOrientation() {
+    val me = Auther("Yuexiang", "Chinese", "Programmer", "ThoughtWorks")
+    println("I'm ${me.name}, a ${me.rate}, a ${me.profession} working for ${me.company}.")
 }
---
+++ src/cn/gaoyuexiang/kotlin/practice/about/Auther.kt
@@ -0,0 +1,4 @@
+package cn.gaoyuexiang.kotlin.practice.about
+
+class Auther(val name: String, val rate: String, var profession: String, var company: String) {
+}
