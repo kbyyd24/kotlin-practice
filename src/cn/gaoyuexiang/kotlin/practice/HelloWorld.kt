bb83addc13a1bed4aaa6c69d2bf4a9df3778771a
kbyyd24 <melo@gaoyuexiang.cn>
2019-05-08 00:06:14 +0800

    add factory method

--- src/cn/gaoyuexiang/kotlin/practice/HelloWorld.kt
+++ src/cn/gaoyuexiang/kotlin/practice/HelloWorld.kt
@@ -2,6 +2,7 @@ package cn.gaoyuexiang.kotlin.practice
 
 import cn.gaoyuexiang.kotlin.practice.Color.GREEN
 import cn.gaoyuexiang.kotlin.practice.about.Auther
+import cn.gaoyuexiang.kotlin.practice.about.createChinese
 import cn.gaoyuexiang.kotlin.practice.extensions.lastChar
 
 fun main() {
@@ -16,4 +17,7 @@ fun main() {
 fun objectOrientation() {
     val me = Auther("Yuexiang", "Chinese", "Programmer", "ThoughtWorks")
     println("I'm ${me.name}, a ${me.rate}, a ${me.profession} working for ${me.company}.")
+
+    val anotherMe = createChinese("Yuexiang")
+    println("I'm ${anotherMe.name}, a ${anotherMe.rate}, a ${anotherMe.profession} working for ${anotherMe.company}.")
 }
--- src/cn/gaoyuexiang/kotlin/practice/about/Auther.kt
+++ src/cn/gaoyuexiang/kotlin/practice/about/Auther.kt
@@ -1,4 +1,8 @@
 package cn.gaoyuexiang.kotlin.practice.about
 
-class Auther(val name: String, val rate: String, var profession: String, var company: String) {
+class Auther(val name: String, val rate: String, var profession: String?, var company: String?) {
+}
+
+fun createChinese(name: String, profession: String? = null, company: String? = null): Auther {
+    return Auther(name, "Chinese", profession, company)
 }
